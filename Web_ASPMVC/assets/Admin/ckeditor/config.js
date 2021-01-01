a61de9f6fb588e17b81079564d0aed548372849f
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-01 22:31:18 +0700

    .

--- Web_ASPMVC/assets/Admin/ckeditor/config.js
+++ Web_ASPMVC/assets/Admin/ckeditor/config.js
@@ -5,9 +5,9 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
     // config.uiColor = '#AADC6E';
     config.syntaxhighlight_lang = 'csharp';
     config.syntaxhighlight_hideControls = true;
