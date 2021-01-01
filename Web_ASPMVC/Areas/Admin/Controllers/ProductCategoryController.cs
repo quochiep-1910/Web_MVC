a45164bbeca308320ea3d7fae358b781180f1349
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-01 22:32:50 +0700

    Create Product Category

---
+++ Web_ASPMVC/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_ASPMVC.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        // GET: Admin/ProductCategory
+        public ActionResult Index()
+        {
+            var dao = new ProductCategoryDAO();
+            var model = dao.ListAll();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
