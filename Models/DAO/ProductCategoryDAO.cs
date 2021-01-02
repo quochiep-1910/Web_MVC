b83faa9b8bd2688da8a87747af3f4b419848da46
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-02 10:12:14 +0700

    update producte Category

--- Models/DAO/ProductCategoryDAO.cs
+++ Models/DAO/ProductCategoryDAO.cs
@@ -1,4 +1,6 @@
 ﻿using Models.EF;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,14 +15,80 @@ namespace Models.DAO
             db = new Web_MVC();
         }
 
+        public long Insert(ProductCategory entity)
+        {
+            entity.CreatedDate = DateTime.Now;
+            db.ProductCategories.Add(entity);//truyền vào tham số rồi tự động add vào data thông qua SaveChanges
+            db.SaveChanges();
+            return entity.ID; //tự sinh ra ID
+        }
+
         public List<ProductCategory> ListAll()
         {
             return db.ProductCategories.Where(x => x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
         }
 
+        public IEnumerable<ProductCategory> ListAllPaging(string search, int page, int pageSize) //lấy ra danh sách trong ProductCategory
+        {
+            //có thể thêm 1 vài tham số tìm kiếm bằng cách thêm else if
+            IQueryable<ProductCategory> model = db.ProductCategories; //IQueryable là duyệt phần tử theo chiều tiến lên theo thứ tự
+            if (!string.IsNullOrEmpty(search))//kiểm tra rỗng hoặc null
+            {
+                model = model.Where(x => x.Name.Contains(search));    //tìm theo username và name( gần đúng or đúng)
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);    //lấy ra số bản ghi của database và sắp xếp theo thứ tự ngày tạo  và sắp xếp giảm dần(OrderByDescending)
+        }
+
         public ProductCategory ViewDetail(long id)
         {
             return db.ProductCategories.Find(id);
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var productCategory = db.ProductCategories.Find(id);
+                db.ProductCategories.Remove(productCategory);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(ProductCategory entity)
+        {
+            try
+            {
+                var productcategory = db.ProductCategories.Find(entity.ID); //var ra thực thể gán vào đối tượng
+                productcategory.Name = entity.Name;
+                productcategory.MetaTitle = entity.MetaTitle;
+                productcategory.ParentID = entity.ParentID;
+                productcategory.DisplayOrder = entity.DisplayOrder;
+                productcategory.SeoTitle = entity.SeoTitle;
+                productcategory.ModifiedDate = DateTime.Now;
+                productcategory.MetaKeywords = entity.MetaKeywords;
+                productcategory.MetaDescriptions = entity.MetaDescriptions;
+                productcategory.Status = entity.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return false;
+            }
+        }
+
+        public bool ChangeStatus(long id)
+        {
+            var productCategory = db.ProductCategories.Find(id);
+            productCategory.Status = !productCategory.Status; //cứ giá trị nào trong status là true thì chuyển thành false (đảo ngược giá trị)
+            db.SaveChanges();
+            return productCategory.Status;
         }
     }
 }
--- Web_ASPMVC/Areas/Admin/Controllers/ProductCategoryController.cs
+++ Web_ASPMVC/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Models.DAO;
+using Models.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,81 @@ namespace Web_ASPMVC.Areas.Admin.Controllers
     public class ProductCategoryController : BaseController
     {
         // GET: Admin/ProductCategory
-        public ActionResult Index()
+        public ActionResult Index(string search, int page = 1, int pageSize = 4)
         {
             var dao = new ProductCategoryDAO();
-            var model = dao.ListAll();
+            var model = dao.ListAllPaging(search, page, pageSize);//truyền page và pageSize vàoo
+            ViewBag.search = search;
             return View(model);
         }
 
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)//kiểm tra rỗng
+            {
+                var dao = new ProductCategoryDAO();
+                long id = dao.Insert(productCategory); //gán biến id vào DAO
+                if (id > 0) //nếu insert thành công thì id>0
+                {
+                    SetAlert("Thêm danh mục sản phẩm thành công", "success");
+                    return RedirectToAction("Index", "ProductCategory");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm danh mục sản phẩm thất bại");
+                }
+            }
+            return View("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var user = new ProductCategoryDAO().ViewDetail(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)//kiểm tra rỗng
+            {
+                var dao = new ProductCategoryDAO();
+                var result = dao.Update(productCategory); //gán biến id vào DAO
+                if (result) //nếu insert thành công thì id>0
+                {
+                    SetAlert("Sửa danh mục sản phẩm thành công", "success");
+                    return RedirectToAction("Index", "ProductCategory");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập Nhập thất bại");
+                }
+            }
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new ProductCategoryDAO().Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public JsonResult ChangeStatus(long id) //id trong Models.EF.User truyền kiểu gì thì truyền y như vậy
+        {
+            var result = new ProductCategoryDAO().ChangeStatus(id);
+            return Json(new
+            {
+                status = result  //trả về đối tượng json
+            });
         }
     }
 }
